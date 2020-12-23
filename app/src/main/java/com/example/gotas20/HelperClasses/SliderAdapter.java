da8eaa44738a428efdaee989bcf83f1a3089bb1b
shofurafm <shofurafm@gmail.com>
2020-12-23 12:13:15 +0700

    add new SliderAdapter for OnBoardingPage

---
+++ app/src/main/java/com/example/gotas20/HelperClasses/SliderAdapter.java
@@ -0,0 +1,78 @@
+package com.example.gotas20.HelperClasses;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.gotas20.R;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    int images[] = {
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background
+    };
+
+    int headings[] = {
+      R.string.ob_heading_1,
+      R.string.ob_heading_2,
+      R.string.ob_heading_3
+    };
+
+    int descriptions[] = {
+            R.string.ob_desc_1,
+            R.string.ob_desc_2,
+            R.string.ob_desc_3
+    };
+
+    @Override
+    public int getCount() {
+        return headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view ==  (ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slides_layout, container, false);
+
+        //Hooks
+        ImageView imageView = view.findViewById(R.id.slider_img);
+        TextView heading = view.findViewById(R.id.slider_heading);
+        TextView desc = view.findViewById(R.id.slider_desc);
+
+        imageView.setImageResource(images[position]);
+        heading.setText(headings[position]);
+        desc.setText(descriptions[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout)object);
+    }
+}
--- app/src/main/java/com/example/gotas20/OnBoardingPage.java
+++ app/src/main/java/com/example/gotas20/OnBoardingPage.java
@@ -1,14 +1,73 @@
 package com.example.gotas20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.gotas20.HelperClasses.SliderAdapter;
 
 public class OnBoardingPage extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    SliderAdapter sliderAdapt;
+    TextView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding_page);
+
+        //Hooks
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+
+        //Call Adapter
+        sliderAdapt = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapt);
+
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
     }
+
+    private void addDots(int position){
+
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+
+        for(int i=0; i<dots.length; i++){
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(45);
+
+            dotsLayout.addView(dots[i]);
+        }
+
+        if(dots.length>0){
+            dots[position].setTextColor(getResources().getColor(R.color.darkOrange));
+        }
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            addDots(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
