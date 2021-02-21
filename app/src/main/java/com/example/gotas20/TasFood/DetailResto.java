cf3b38825dcf058fa07c23fcb6203c68404881a0
shofurafm <shofurafm@gmail.com>
2021-02-21 20:18:36 +0700

    detail resaurant for tasfood

---
+++ app/src/main/java/com/example/gotas20/TasFood/DetailResto.java
@@ -0,0 +1,14 @@
+package com.example.gotas20.TasFood;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.gotas20.R;
+import android.os.Bundle;
+
+public class DetailResto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_resto);
+    }
+}
